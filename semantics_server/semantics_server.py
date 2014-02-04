86bb92eb3960ae4f7c991c5a5643e607d2c61a4a
Adabriand <adabriand@gmail.com>
2014-02-04 11:21:12 -0300

    Added FB templates

--- semantics_server/semantics_server.py
+++ semantics_server/semantics_server.py
@@ -1,3 +1,4 @@
+import os
 import random
 import sys
 import pbclient
@@ -9,7 +10,7 @@ app = Flask(__name__)
 #internal methods ##
 #####################
 def get_connection():
-    cnx = mysql.connector.connect(user='root', password='S0cye!#02-@gH',
+    cnx = mysql.connector.connect(user='root', password='',
                                 host='localhost',
                                 database='semantics');
 
@@ -60,7 +61,7 @@ def check_connection():
         query = ("SELECT count(*) FROM words");
         cursor.execute(query);
         rows = cursor.fetchall();
-
+        
         cursor.close();
         cnx.close();
         return "database ok!"
@@ -84,6 +85,8 @@ def linkWords(word):
         words = cursor.fetchall();        
         cursor.close();
         cnx.close();
+        
+        print(words)
 
         if (len(words)>0):            
             result = str(len(words));
@@ -100,46 +103,49 @@ def linkWords(word):
 @app.route('/setup')
 def setup():    
     # settings
-    pbclient.set('api_key', "74690b3e-e980-4299-b006-9c6a5c50b355")
-    pbclient.set('endpoint', 'http://pybossa.socientize.eu/pybossa')
+    pbclient.set('api_key', "c008cbd5-7885-4c49-a0fe-6cdee926651f")
+    pbclient.set('endpoint', 'http://localhost/pybossa')
 
     # Create the app
     #pbclient.create_app('Semantics Map','Semantics','What is the perceived relation between words? ');
+    dir = os.path.dirname(__file__)
+        
     #update app
-    pyBossaApp = pbclient.find_app(short_name='Semantics')[0];
-    #pyBossaApp.long_description = '- add long description -';
-    #pyBossaApp.info['task_presenter'] = contents('template.html')
-    #pyBossaApp.info['thumbnail'] = "http://societic.ibercivis.es/semantics/static/images/icon.jpg"
+    pyBossaApp = pbclient.find_app(short_name='Semantics')[0]
+    pyBossaApp.long_description = contents(dir + '/../View/long_description.html')
+    pyBossaApp.info['task_presenter'] = contents(dir + '/../View/template.html')
+    pyBossaApp.info['thumbnail'] = "http://societic.ibercivis.es/semantics/static/images/icon.jpg"
     #pyBossaApp.info['tutorial'] = contents('tutorial.html')
-    #pbclient.update_app(pyBossaApp)
+    pyBossaApp.category_id = 1
+    pbclient.update_app(pyBossaApp)
 
     #create tasks
     try:
-        cnx = get_connection();
+        cnx = get_connection()
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            return "Something is wrong your username or password";
+            return "Something is wrong your username or password"
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                return "Database does not exists";
+                return "Database does not exists"
         else:
             return err;
     else:
         #Get Data
-        cursor = cnx.cursor();
-        cursor.execute("SELECT * FROM startwords");
-        words = cursor.fetchall();        
-        cursor.close();
-        cnx.close();
+        cursor = cnx.cursor()
+        cursor.execute("SELECT * FROM startwords")
+        words = cursor.fetchall()        
+        cursor.close()
+        cnx.close()
 
-        if (len(words)>0):            
-            for item in words:
-                    task_info = dict(start=item[0],
-                        end=item[1],
-                        startWord=getWord(item[0]),
-                        endWord=getWord(item[1]) )
-                    pbclient.create_task(pyBossaApp.id, task_info)
+        #if (len(words)>0):            
+        #    for item in words:
+        #            task_info = dict(start=item[0],
+        #                end=item[1],
+        #                startWord=getWord(item[0]),
+        #                endWord=getWord(item[1]) )
+        #            pbclient.create_task(pyBossaApp.id, task_info)
         
-    return "ok";
+    return "ok"
 
 if __name__ == '__main__':
     app.run()
